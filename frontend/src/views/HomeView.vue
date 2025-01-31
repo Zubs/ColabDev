c983984ff669d05d4ecf929ae953cd1180292525
Zubs <zubairidrisaweda@gmail.com>
2025-01-31 18:09:09 +0000

    feat: landing page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,29 @@
 <template>
     <NavBar />
+    <Hero />
+    <Services />
+    <FunFacts />
+    <Speakers />
+    <Events />
+    <Testimonies />
+    <Pricing />
+    <Blogs />
+    <Newsletter />
+    <Gallery />
+    <Footer />
 </template>
 
 <script setup>
 import NavBar from "@/components/NavBar.vue";
+import Hero from "@/components/Hero.vue";
+import Services from "@/components/Services.vue";
+import FunFacts from "@/components/FunFacts.vue";
+import Speakers from "@/components/Speakers.vue";
+import Events from "@/components/Events.vue";
+import Testimonies from "@/components/Testimonies.vue";
+import Pricing from "@/components/Pricing.vue";
+import Blogs from "@/components/Blogs.vue";
+import Newsletter from "@/components/Newsletter.vue";
+import Gallery from "@/components/Gallery.vue";
+import Footer from "@/components/Footer.vue";
 </script>
