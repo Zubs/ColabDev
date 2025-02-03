6e3a2264e0c1f9412f001f77087ff6f14fa09973
Zubs <zubairidrisaweda@gmail.com>
2025-02-03 16:46:13 +0000

    init registration

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,23 +1,26 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-      <a class="navbar-brand" href="index.html"><img src="https://www.wlv.ac.uk/media/2019-template-assets/graphics/logo.svg" alt="University Of Wolverhampton Logo"></a>
+      <a class="navbar-brand" href="/"><img src="https://www.wlv.ac.uk/media/2019-template-assets/graphics/logo.svg" alt="University Of Wolverhampton Logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="oi oi-menu"></span> Menu
       </button>
 
       <div class="collapse navbar-collapse" id="ftco-nav">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-          <li class="nav-item"><a href="speakers.html" class="nav-link">Speakers</a></li>
-          <li class="nav-item"><a href="schedule.html" class="nav-link">Schedule</a></li>
-          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-          <li class="nav-item cta mr-md-2"><a href="#" class="nav-link">Book Now</a></li>
+          <li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="/" class="nav-link">About</a></li>
+          <li class="nav-item"><a href="/" class="nav-link">Speakers</a></li>
+          <li class="nav-item"><a href="/" class="nav-link">Schedule</a></li>
+          <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
+          <li class="nav-item"><a href="/" class="nav-link">Contact</a></li>
+          <li class="nav-item cta mr-md-2"><a href="/registration" class="nav-link">Book Now</a></li>
 
         </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+</script>
