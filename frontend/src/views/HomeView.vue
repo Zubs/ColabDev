d9c5194e30590b2b7bd202b87c713944c58c418f
Zubair Idris Aweda <zubairidrisaweda@gmail.com>
2025-03-17 06:10:45 +0000

    .

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,12 +2,10 @@
     <NavBar />
     <Hero />
     <Services />
-    <FunFacts />
     <Speakers />
     <Events />
     <Testimonies />
     <Pricing />
-    <BlogsComponent />
     <Newsletter />
     <Gallery />
     <Footer />
@@ -17,12 +15,10 @@
 import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
 import Services from "@/components/Services.vue";
-import FunFacts from "@/components/FunFacts.vue";
 import Speakers from "@/components/Speakers.vue";
 import Events from "@/components/Events.vue";
 import Testimonies from "@/components/Testimonies.vue";
 import Pricing from "@/components/Pricing.vue";
-import BlogsComponent from "@/components/BlogsComponent.vue";
 import Newsletter from "@/components/Newsletter.vue";
 import Gallery from "@/components/Gallery.vue";
 import Footer from "@/components/Footer.vue";
